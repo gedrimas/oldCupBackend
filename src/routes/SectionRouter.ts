d55365b696e003e3ab2d503a9211f5740dbe742e
Artur Gedrimas <arturgedrimas@MacBook-Air-Artur.local>
2021-06-08 23:09:49 +0300

    section deletion

--- src/routes/SectionRouter.ts
+++ src/routes/SectionRouter.ts
@@ -1,14 +1,14 @@
-import { Request, Response, Router, NextFunction } from 'express';
-import StatusCodes from 'http-status-codes';
-import { 
-  sectionAddingError, 
-  sectionUpdatingError, 
-  sectionAndAdvertsDeletionError 
-} from '@shared/constants';
-import Section from '../models/section';
-import Advert from '../models/advert';
-import { adminMW } from './middleware';
-import { ErrorWithStatus, updateIfNewValueProvided } from '@shared/functions';
+import { Request, Response, Router, NextFunction } from "express";
+import StatusCodes from "http-status-codes";
+import {
+  sectionAddingError,
+  sectionUpdatingError,
+  sectionAndAdvertsDeletionError,
+} from "@shared/constants";
+import Section from "../models/section";
+import Advert from "../models/advert";
+import { adminMW } from "./middleware";
+import { ErrorWithStatus, updateIfNewValueProvided } from "@shared/functions";
 
 const SectionRouter = Router();
 
@@ -17,7 +17,7 @@ const { OK, CREATED, NOT_IMPLEMENTED } = StatusCodes;
 /******************************************************************************
  *                      Section - "POST /section"
  ******************************************************************************/
-SectionRouter.route('/')
+SectionRouter.route("/")
   .post(adminMW, (req: Request, res: Response, next: NextFunction) => {
     //Create new section
     Section.create(req.body)
@@ -47,28 +47,32 @@ SectionRouter.route('/')
         const err = new ErrorWithStatus(NOT_IMPLEMENTED, sectionUpdatingError);
         return next(err);
       });
-  })
-    /******************************************************************************
-   *       Section - "Delete sectins and all adverts in that sectin/section"
-   ******************************************************************************/
-     .delete(adminMW, (req: Request, res: Response, next: NextFunction) => {
-      //Find and delete related adverts
-      Advert.deleteMany({
-        sectionId: req.body.sectionId
-      })
-        .then(() => {
-          //Find and delete section
-          Section.deleteOne({_id: req.body.sectionId})
-            .then(() => {
-              return res.status(OK).end();
-            })
-        })
-        .catch(() => {
-          //Return Error if section and adverts wasn't deleted 
-          const err = new ErrorWithStatus(NOT_IMPLEMENTED, sectionAndAdvertsDeletionError);
-          return next(err);
-        });
+  });
+/******************************************************************************
+ *       Section - "Delete sectins and all adverts in that sectin/section"
+ ******************************************************************************/
+SectionRouter.route("/:sectionId").delete(
+  adminMW,
+  (req: Request, res: Response, next: NextFunction) => {
+    //Find and delete related adverts
+    Advert.deleteMany({
+      sectionId: req.params.sectionId,
     })
-
+      .then(() => {
+        //Find and delete section
+        Section.deleteOne({ _id: req.params.sectionId }).then(() => {
+          return res.status(OK).end();
+        });
+      })
+      .catch(() => {
+        //Return Error if section and adverts wasn't deleted
+        const err = new ErrorWithStatus(
+          NOT_IMPLEMENTED,
+          sectionAndAdvertsDeletionError
+        );
+        return next(err);
+      });
+  }
+);
 
 export default SectionRouter;
